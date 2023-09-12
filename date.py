d16d585d67c8c0e39fd723d47da74f41ab229e6a
Kedmond24 <Kedmond24@live.com>
2023-09-12 17:43:56 -0400

    ADD script description

--- date.py
+++ date.py
@@ -1,8 +1,11 @@
 ##############################################################################################################################
 # Created by Kevin Edmond.
 #
-# To Be Updated
-# To Be Updated
+# This python script is used to simulate two people on a date. You begin by providing your date's name and your starting budget.
+# Next, you provide the level (1-3) of restaurants you would like to attend. Menu prices for each restaurant is relative to the
+# restaurant level. At the restaurant, your and your date provide a meal and drink selection from the provided menu. When the
+# meal is complete, you receive an order total which is then subtracted from your budget. Outside of the restaurant, you check
+# your budget and ask for a second date. Depending on the remaining budget, you will receive one of three responses.
 #
 #
 # Please read README.md for full notes on script.
@@ -11,9 +14,14 @@
 
 from time import sleep
 
+
+# Get your name
+my_name = input("Your Name: ").capitalize()
+
+# Get date's name
 date_name = input("Date's Name: ").capitalize()
 
-# starting budget
+# Starting budget
 budget = int(input('Enter Budget: '))
 
 # Select Restaurant Star Level
@@ -74,23 +82,25 @@ print(" ***** ***** ***** ***** ***** ")
 print(f"    Welecome to {restaurants[restaurant_level]['name']}!")
 print(" ***** ***** ***** ***** ***** ")
 print(" ")
+print(f"Waiter: Hello, {my_name} and {date_name}!")
 print("Waiter: Can I take your order?")
+sleep(1)
 
-# Show menu
+# Show food menu
 print(restaurants[restaurant_level]['menu'])
 
-# Get menu orders
+# Get food menu orders
 my_menu_order = input("Your Menu Order: ").lower()
 order_total += restaurants[restaurant_level]['menu'][my_menu_order]
 
 date_menu_order = input("Date's Menu Order: ").lower()
 order_total += restaurants[restaurant_level]['menu'][date_menu_order]
 
-# Get Drink Orders
+# Show drink menu
 print("...and what would you like to drink?")
 print(restaurants[restaurant_level]['drink'])
 
-# Get Drink Orders
+# Get drink menu orders
 my_drink_order = input("Your Menu Order: ").lower()
 order_total += restaurants[restaurant_level]['drink'][my_drink_order]
 
@@ -106,23 +116,29 @@ print(f"Your total bill will be ${order_total}!")
 print("Paying for meal...")
 budget -= order_total
 sleep(2)
-(f"Leaving {restaurants[restaurant_level]['name']}...")
+print(f"Leaving {restaurants[restaurant_level]['name']}...")
+print(" ")
+sleep(2)
 
 # Show Remaining Budget
-print("Checking bank app...")
-print(f"You have ${budget} in your account.")
+print("Checking your Hell's Fargo app...")
+print(f"Hi {my_name}, you have ${budget} in your account.")
 sleep(2)
 
 
-print(f"Hey {date_name}, want to go out again sometime?")
+print(f"{my_name}: Hey {date_name}, want to go out again sometime?")
 sleep(1)
 if budget < 50:
-    print("Nah... I'm Good!")
+    print(f"{date_name}: Nah... I'm Good!")
+    print(f"{date_name}: But...")
+    print(" *Tires Screeching* ")
     sleep(1)
-    print("But... *You drive off in your Little Red Corvette*")
+    print(f"{my_name} drives off in their Little Red Corvette...")
 elif budget >= 50 and budget <= 75:
-    print("Call me tomorrow!")
-    print("I'll see if I can clear my schedule.")
+    print(f"{date_name}: Call me tomorrow!")
+    sleep(1)
+    print(f"{date_name}: I'll see if I can clear my schedule.")
 else:
-    print("Definitely!")
-    print("What time are you coming over?")
+    print(f"{date_name}: Definitely!")
+    sleep(1)
+    print(f"{date_name}: What time are you coming over?")
